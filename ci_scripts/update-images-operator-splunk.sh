9dc0f9f9af7ee543d1ac40f1d57018bbfb64f2b2
jvoravong <joshvoravong@gmail.com>
2023-11-10 11:48:25 -0700

    checkpoint

--- ci_scripts/update-images-operator-splunk.sh
+++ ci_scripts/update-images-operator-splunk.sh
@@ -35,9 +35,21 @@ get_latest_tag() {
 
     # Default for Docker Hub
     else
-        local latest_api="https://registry.hub.docker.com/v2/repositories/$repo_value/tags/?page_size=2"  # Fetch only the two latest tags
-        echo $(curl -sL "$latest_api" | jq -r '.results[] | select(.name != "latest") | .name')
-    fi
+      # Trim 'docker.io/' from the start of repo_value if it exists
+      repo_value="${repo_value#docker.io/}"
+      local tags_api="https://registry.hub.docker.com/v2/repositories/$repo_value/tags/?page_size=100"  # Fetch a list of tags, increase page_size if needed
+      # Get the digest of the 'latest' tag if it exists
+      local latest_digest=$(curl -sL "$tags_api" | jq -r '.results[] | select(.name == "latest").images[0].digest' | head -1)
+      # If 'latest' tag has a digest, find the first non-latest tag with the same digest
+      if [[ -n "$latest_digest" ]]; then
+          local non_latest_tag=$(curl -sL "$tags_api" | jq -r --arg digest "$latest_digest" '.results[] | select(.name != "latest" and .images[0].digest == $digest).name' | head -1)
+          echo "$non_latest_tag"
+      else
+          # If 'latest' tag does not exist or has no digest, get the most recent tag
+          local most_recent_tag=$(curl -sL "$tags_api" | jq -r '.results[] | .name' | head -1)
+          echo "$most_recent_tag"
+      fi
+  fi
 }
 
 # ---- Validate Input Arguments ----
@@ -49,28 +61,49 @@ if [ "$#" -eq 0 ]; then
 fi
 
 # ---- Initialize Variables ----
-# Set the instrumentation library name
+# Set the YAML file path and the YAML value path
 setd "YAML_FILE_PATH" "$1"
 setd "YAML_VALUE_PATH" "$2"
 
-# Check if YAML_VALUE_PATH points to a string (Docker image reference)
-if [[ "$(yq eval ".${YAML_VALUE_PATH} | type" "${YAML_FILE_PATH}")" == "string" ]]; then
+# Determine the type of the value at YAML_VALUE_PATH
+if [[ "$YAML_VALUE_PATH" =~ ^select\(.*\) || "$YAML_VALUE_PATH" =~ ^\..* ]]; then
+  # The YAML_VALUE_PATH is a complex yq expression or a direct path starting with a dot
+  # Use 'eval_all' to iterate over all documents in the YAML file
+  value_type="$(yq eval-all "${YAML_VALUE_PATH} | type" "${YAML_FILE_PATH}")"
+else
+  # The YAML_VALUE_PATH is a direct path without a leading dot, so prepend one
+  # Use 'eval_all' to iterate over all documents in the YAML file
+  value_type="$(yq eval-all ".${YAML_VALUE_PATH} | type" "${YAML_FILE_PATH}")"
+fi
+debug value_type
+
+# Check the obtained value type for the specified path
+if [[ "$value_type" == *'str'* ]]; then
   # If it's a string, we assume it's a Docker image reference and parse the repository and tag from it
-  setd "DOCKER_IMAGE_REF" "$(yq eval ".${YAML_VALUE_PATH}" "${YAML_FILE_PATH}")"
+  setd "DOCKER_IMAGE_REF" "$(yq eval "${YAML_VALUE_PATH}" "${YAML_FILE_PATH}")"
   setd "IMAGE_REPOSITORY" "${DOCKER_IMAGE_REF%:*}"
   setd "TAG_LOCAL" "${DOCKER_IMAGE_REF##*:}"
+elif [[ "$value_type" == *'map'* ]]; then
+  # If it's a map, we assume it contains 'repository' and 'tag'
+  setd "IMAGE_REPOSITORY" "$(yq eval "${YAML_VALUE_PATH}.repository" "${YAML_FILE_PATH}")"
+  setd "TAG_LOCAL" "$(yq eval "${YAML_VALUE_PATH}.tag" "${YAML_FILE_PATH}")"
 else
-  # Otherwise, we assume it's a map containing 'repository' and 'tag'
-  setd "IMAGE_REPOSITORY" "$(yq eval ".${YAML_VALUE_PATH}.repository" "${YAML_FILE_PATH}")"
-  setd "TAG_LOCAL" "$(yq eval ".${YAML_VALUE_PATH}.tag" "${YAML_FILE_PATH}")"
+  echo "Error: Unsupported type encountered: $value_type"
+  exit 1
+fi
+
+# Handle special case for networkExplorer
+if [[ "$YAML_VALUE_PATH" =~ networkExplorer ]]; then
+  IMAGE_REPOSITORY="$IMAGE_REPOSITORY/splunk-network-explorer-kernel-collector"
 fi
 
 # ---- Fetch Latest Version ----
-# Fetch the latest version from GitHub
+# Fetch the latest version from the appropriate repository
 setd "TAG_UPSTREAM" "$(get_latest_tag "$IMAGE_REPOSITORY")"
-# TODO: Remove this special case to support networkExplorer
-if [[ "$YAML_VALUE_PATH" =~ networkExplorer ]]; then
-  setd "TAG_UPSTREAM" "$(get_latest_tag "$IMAGE_REPOSITORY/splunk-network-explorer-kernel-collector")"
+# Check if the TAG_UPSTREAM is empty or null
+if [[ -z "$TAG_UPSTREAM" || "$TAG_UPSTREAM" == "null" ]]; then
+  echo "Error: Unable to retrieve the latest tag for $IMAGE_REPOSITORY"
+  exit 1
 fi
 
 # ---- Display Version Information ----
@@ -90,12 +123,25 @@ elif [[ -z "$TAG_LOCAL" || "$TAG_LOCAL" == "null" || "$TAG_LOCAL" != "$TAG_UPSTR
   setd "VALUES_FILE_START_OFFSET" "$VALUES_FILE_START_OFFSET"
 
   # Determine the line number of the tag within the YAML structure, adjusted for the file's starting offset.
-  RELATIVE_LINE_NUM=$(yq eval ".${YAML_VALUE_PATH}.tag | line" "${YAML_FILE_PATH}")
+  # Check if the YAML_VALUE_PATH is a complex query or a direct path
+  if [[ "$YAML_VALUE_PATH" =~ ^select\(.*\) || "$YAML_VALUE_PATH" =~ ^\..* ]]; then
+    # The YAML_VALUE_PATH is a complex yq expression or a direct path starting with a dot
+    # Use 'eval_all' to iterate over all documents in the YAML file
+    setd "RELATIVE_LINE_NUM" "$(yq eval-all "${YAML_VALUE_PATH} | line" "${YAML_FILE_PATH}")"
+  else
+    # The YAML_VALUE_PATH is a direct path without a leading dot, so prepend one
+    # Use 'eval_all' to iterate over all documents in the YAML file
+    setd "RELATIVE_LINE_NUM" "$(yq eval-all ".${YAML_VALUE_PATH} | line" "${YAML_FILE_PATH}")"
+  fi
   LINE_NUM=$(( RELATIVE_LINE_NUM + VALUES_FILE_START_OFFSET ))
   setd "LINE_NUM" "$LINE_NUM"
 
-  # Replace only the tag value on the identified line, preserving the existing formatting and indentation.
-  awk -v LINE_NUM="$LINE_NUM" -v NEW_TAG="$TAG_UPSTREAM" 'NR == LINE_NUM {sub(/tag: .+$/, "tag: " NEW_TAG "")} 1' "${YAML_FILE_PATH}" > temp_file.yaml && mv temp_file.yaml "${YAML_FILE_PATH}"
+  awk -v LINE_NUM="$LINE_NUM" -v CURRENT_TAG="$TAG_LOCAL" -v NEW_TAG="$TAG_UPSTREAM" '
+    NR == LINE_NUM {
+      # Replace only the CURRENT_TAG with NEW_TAG, preserving the rest of the line
+      gsub(CURRENT_TAG, NEW_TAG)
+    }
+    { print }' "${YAML_FILE_PATH}" > temp_file.yaml && mv temp_file.yaml "${YAML_FILE_PATH}"
 
   NEED_UPDATE=1  # Setting NEED_UPDATE to 1 as an update is required
   debug "Tag updated from $TAG_LOCAL to $TAG_UPSTREAM, NEED_UPDATE set to $NEED_UPDATE"
