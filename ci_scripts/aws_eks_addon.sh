9bcad20e8cf722a97dd8baf31bad6cad20fda8cc
jvoravong <joshvoravong@gmail.com>
2023-11-20 10:40:42 -0700

    checkpoint v0.86.62

--- ci_scripts/aws_eks_addon.sh
+++ ci_scripts/aws_eks_addon.sh
@@ -9,9 +9,9 @@ source "$SCRIPT_DIR/base_util.sh"
 # Set source and destination repository and tags
 SRC_REPO="quay.io/signalfx/splunk-otel-collector"
 SRC_TAG="0.86.0"
-DST_REPO="709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk/splunk-otel-collector-app"
+DST_REG="709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk"
+DST_REPO=$DST_REG/splunk-otel-collector-app
 DST_TAG=$(grep "^version:" $SCRIPT_DIR/../helm-charts/splunk-otel-collector/Chart.yaml | awk '{print $2}')
-export AWS_PROFILE=marketplace
 
 if [ -z "$DST_TAG" ]; then
     echo "Destination tag (DST_TAG) is empty. Please check the Helm chart version."
@@ -24,56 +24,93 @@ if ! command -v skopeo &> /dev/null; then
     exit 1
 fi
 
-# Log in to the source repository if needed
-# echo "Logging in to source repository..."
-# skopeo login quay.io -u your-username -p your-password
+echo "Authenticating with Docker, Skopeo (Docker multi-arch power tool), and Helm with AWS ECR credentials..."
+export AWS_PROFILE=marketplace
 
-# Log in to the destination repository if needed
-# echo "Logging in to destination repository..."
-aws ecr get-login-password --region us-east-1 | skopeo login --username AWS --password-stdin $DST_REPO
-
-# Copy the multi-architecture image from the source to the destination
-echo "Copying multi-architecture image from $SRC_REPO:$SRC_TAG to $DST_REPO:$DST_TAG..."
-skopeo copy --all "docker://$SRC_REPO:$SRC_TAG" "docker://$DST_REPO:$DST_TAG"
-
-if [ $? -ne 0 ]; then
-    echo "Failed to copy the image."
+# Docker Login
+if aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$DST_REPO"; then
+    echo "Docker login successful."
+else
+    echo "Docker login failed."
     exit 1
+fi
+
+# Skopeo Login
+if aws ecr get-login-password --region us-east-1 | skopeo login --username AWS --password-stdin "$DST_REPO"; then
+    echo "Skopeo login successful."
+else
+    echo "Skopeo login failed."
+    exit 1
+fi
+
+# Helm Registry Login
+export HELM_EXPERIMENTAL_OCI=1
+if aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin "$DST_REG"; then
+    echo "Helm registry login successful."
+else
+    echo "Helm registry login failed."
+    exit 1
+fi
+
+# Check if the destination image already exists and compare digests
+echo "Checking if the destination image already exists..."
+if docker manifest inspect "$DST_REPO:$DST_TAG" &> /dev/null; then
+  SRC_MANIFEST=$(docker manifest inspect "$SRC_REPO:$SRC_TAG")
+  DST_MANIFEST=$(docker manifest inspect "$DST_REPO:$DST_TAG")
+
+  SRC_DIGESTS=$(echo "$SRC_MANIFEST" | jq -r '.manifests[].digest')
+  DST_DIGESTS=$(echo "$DST_MANIFEST" | jq -r '.manifests[].digest')
+
+  # Sort and compare the digests
+  SORTED_SRC_DIGESTS=$(echo "$SRC_DIGESTS" | sort)
+  SORTED_DST_DIGESTS=$(echo "$DST_DIGESTS" | sort)
+
+  if [ "$SORTED_SRC_DIGESTS" == "$SORTED_DST_DIGESTS" ]; then
+      echo "The destination image already exists and has the same digests for all architectures. No need to copy."
+  else
+      echo "The destination image exists but has different digests. Aborting."
+      exit 1
+  fi
+else
+  echo "Destination image does not exist. Proceeding with the copy."
+  # Copy the multi-architecture image from the source to the destination
+  echo "Copying multi-architecture image from $SRC_REPO:$SRC_TAG to $DST_REPO:$DST_TAG..."
+  skopeo copy --all "docker://$SRC_REPO:$SRC_TAG" "docker://$DST_REPO:$DST_TAG"
+
+  if [ $? -ne 0 ]; then
+      echo "Failed to copy the image."
+      exit 1
+  fi
+
+  # Fetch the source and destination manifests
+  SRC_MANIFEST=$(docker manifest inspect $SRC_REPO:$SRC_TAG)
+  DST_MANIFEST=$(docker manifest inspect $DST_REPO:$DST_TAG)
+
+  # Parse the manifests to get the digests for each architecture
+  SRC_DIGESTS=$(echo "$SRC_MANIFEST" | jq -r '.manifests[].digest')
+  DST_DIGESTS=$(echo "$DST_MANIFEST" | jq -r '.manifests[].digest')
+
+  # Sort and compare the digests
+  SORTED_SRC_DIGESTS=$(echo "$SRC_DIGESTS" | sort)
+  SORTED_DST_DIGESTS=$(echo "$DST_DIGESTS" | sort)
+
+  if [ "$SORTED_SRC_DIGESTS" == "$SORTED_DST_DIGESTS" ]; then
+      echo "The source and destination manifests have identical content."
+  else
+      echo "The source and destination manifests do not have identical content."
+      exit 1
+  fi
 fi
 
 echo "Image copy completed successfully."
 
-# Fetch the source and destination manifests
-SRC_MANIFEST=$(docker manifest inspect $SRC_REPO:$SRC_TAG)
-DST_MANIFEST=$(docker manifest inspect $DST_REPO:$DST_TAG)
-
-# Parse the manifests to get the digests for each architecture
-SRC_DIGESTS=$(echo "$SRC_MANIFEST" | jq -r '.manifests[].digest')
-DST_DIGESTS=$(echo "$DST_MANIFEST" | jq -r '.manifests[].digest')
-
-# Sort and compare the digests
-SORTED_SRC_DIGESTS=$(echo "$SRC_DIGESTS" | sort)
-SORTED_DST_DIGESTS=$(echo "$DST_DIGESTS" | sort)
-
-if [ "$SORTED_SRC_DIGESTS" == "$SORTED_DST_DIGESTS" ]; then
-    echo "The source and destination manifests have identical content."
-else
-    echo "The source and destination manifests do not have identical content."
-    exit 1
-fi
-
 # ---- Package the Helm chart and upload to ECR ----
 # Define VERSION for packaging Helm chart
 VERSION=$(grep "^version:" "$SCRIPT_DIR/../helm-charts/splunk-otel-collector/Chart.yaml" | awk '{print $2}')
-DST_REG="709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk"
 if [ -z "$VERSION" ]; then
     echo "Chart version (VERSION) is empty. Please check the Chart.yaml file."
     exit 1
 fi
-
-# Authenticate Docker with ECR
-echo "Logging in to AWS ECR..."
-aws ecr get-login-password --region us-east-1 |helm registry login --username AWS --password-stdin "$DST_REG"
 
 # Configure Helm for ECR
 echo "Configuring Helm for ECR..."
@@ -89,6 +126,7 @@ if [ ! -f "$CHART_FILE" ]; then
     exit 1
 fi
 
+# TODO: Add a check here to see if a the same helm chart docker image has already been uploaded, the naming conflict makes it tricky
 # Push the packaged Helm chart to the repository
 echo "Pushing the Helm chart $CHART_FILE to the repository..."
 helm push "$CHART_FILE" oci://709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk
