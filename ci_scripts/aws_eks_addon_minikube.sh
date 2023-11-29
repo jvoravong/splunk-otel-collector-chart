426c7033032ddb795fa857be085d5c9b5e1a020f
jvoravong <47871238+jvoravong@users.noreply.github.com>
2023-11-29 08:42:06 -0700

    temp

--- ci_scripts/aws_eks_addon_minikube.sh
+++ ci_scripts/aws_eks_addon_minikube.sh
@@ -4,22 +4,44 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/base_util.sh"
 
-# ---- Copy Collector Docker Image from ECR to Minikube ----
+# List of images to be processed
+IMAGES=(
+    "709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk/splunk-otel-collector:0.86.66"
+    "709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk/splunk-otel-collector-app:0.86.66"
+    "709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk/splunk-fluentd-hec:1.3.3"
+    "709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk/splunk-ubi9:9.2-755.1697625012"
+)
 
-# Set source repository and tags
-SRC_REPO="709825985650.dkr.ecr.us-east-1.amazonaws.com/splunk/splunk-otel-collector-app"
-SRC_TAG=$(grep "^version:" "$SCRIPT_DIR/../helm-charts/splunk-otel-collector/Chart.yaml" | awk '{print $2}')
 export AWS_PROFILE=marketplace
-
-# Full image path with tag
-FULL_IMAGE_PATH="${SRC_REPO}:${SRC_TAG}"
 
 # Log in to ECR
 echo "Logging in to AWS ECR..."
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$SRC_REPO"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull the image
-echo "Pulling image ${FULL_IMAGE_PATH}..."
-docker pull "$FULL_IMAGE_PATH"
+# Process each image
+for FULL_IMAGE_PATH in "${IMAGES[@]}"; do
+    echo "Pulling image ${FULL_IMAGE_PATH}..."
+    docker pull "$FULL_IMAGE_PATH"
+
+    # Check if Minikube is running
+    if minikube status &>/dev/null; then
+        echo "Minikube is running. Loading image into Minikube: ${FULL_IMAGE_PATH}"
+        minikube image load "$FULL_IMAGE_PATH"
+        echo "Image loaded into Minikube successfully."
+    else
+        echo "Minikube is not running or not installed."
+    fi
+
+    # Check if Kind is running
+    if kind get clusters &>/dev/null; then
+        for cluster in $(kind get clusters); do
+            echo "Loading image into Kind cluster: $cluster"
+            kind load docker-image "$FULL_IMAGE_PATH" --name "$cluster"
+            echo "Image loaded into Kind cluster $cluster successfully."
+        done
+    else
+        echo "Kind is not running or not installed."
+    fi
+done
 
 echo "All processes completed successfully."
